466447c4d1582156276cca053597737cef22301f
Kenneth Tso <iamkennytso@gmail.com>
2020-06-03 16:53:36 -0400

    progress

--- src/games/battleship/battleship.js
+++ src/games/battleship/battleship.js
@@ -2,24 +2,48 @@ import GameComponent from "../../GameComponent.js";
 import React from "react";
 import UserApi from "../../UserApi.js";
 
+// this.getSessionUserIds() returns array of userIDs in the game
+// this.getMyUserId() returns my userID
+// this.getSessionCreatorUserId() returns session creator userID
+// this.getSessionDatabaseRef().set({}) sets our 'database'
+// onSessionDataChanged(data) ???
+
+//
 export default class TicTacToe extends GameComponent {
   constructor(props) {
     super(props);
     this.state = {
-      message: ""
+      playerTurn: "initial",
+      createAGrid: []
     };
   }
 
-  componentDidMount() {
-    this.getMessage();
+  onSessionDataChanged(data) {
+    console.log("onSessionDataChanged", data);
+    this.setState({ playerTurn: data.playerTurn });
   }
 
-  getMessage() {
-    if (this.getSessionCreatorUserId() === this.getMyUserId()) {
-      this.setState({ message: "Youre the host" });
-    } else {
-      this.setState({ message: "Youre the guest" });
-    }
+  handleStartGameClick() {
+    console.log("click");
+    this.getSessionDatabaseRef().set({
+      playerAGrid: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ],
+      playerBGrid: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ],
+      playerAAttack: 2,
+      playerBAttack: 2,
+      playerTurn: "setup"
+    });
   }
 
   render() {
@@ -29,15 +53,30 @@ export default class TicTacToe extends GameComponent {
     ));
     var creator = UserApi.getName(this.getSessionCreatorUserId());
 
-    return (
-      <div>
-        <p>You're the host</p>
-        <p>You're a guest, welcome!</p>
-        <p>Session ID: {id}</p>
-        <p>Session creator: {creator}</p>
-        <p>Session users:</p>
-        <ul>{users}</ul>
-      </div>
-    );
+    if (this.state.playerTurn === "initial") {
+      return (
+        <div>
+          You are in the {this.state.playerTurn} step
+          <p>My name is: {UserApi.getName(this.getMyUserId())}</p>
+          <p>Session ID: {id}</p>
+          <p>Session creator: {creator}</p>
+          <p>Session users:</p>
+          <ul>{users}</ul>
+          <button onClick={() => this.handleStartGameClick()}>
+            Start Game!
+          </button>
+        </div>
+      );
+    } else if (this.state.playerTurn === "setup") {
+      return (
+        <div>
+          You are in the {this.state.playerTurn} step <br />
+          Here is your grid:
+          {/* {playerAGrid} */}
+        </div>
+      );
+    } else {
+      return <div>THIS SHOULD NEVER HAPPEN</div>;
+    }
   }
 }
